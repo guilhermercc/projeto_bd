1ac955935bf54b2c1c81f701a68df331bc8ae207
brenoasantos <brenoaas2@gmail.com>
2023-09-13 13:53:38 -0300

    new queries and query types

--- 4 - SQL/consultas.sql
+++ 4 - SQL/consultas.sql
@@ -1,4 +1,5 @@
 -- 1. Liste o cpf e o nome dos espectadores que seguem mais de 1 pessoa (HAVING, GROUP BY, INNER JOIN)
+
 SELECT S.CPF_SEGUIDOR, E.NOME, COUNT(*) as NUM_SEGUIDOS 
 FROM SEGUE S
 INNER JOIN ESPECTADOR E
@@ -6,11 +7,14 @@ ON S.CPF_SEGUIDOR = E.CPF
 GROUP BY S.CPF_SEGUIDOR, E.NOME
 HAVING COUNT(*) > 1;
 
--- 2. Encontre os streamers que não possuem canais
+
+-- 2. Encontre os streamers que não possuem canais (SUBCONSULTA TIPO TABELA)
 SELECT * FROM STREAMER 
 WHERE CPF NOT IN (SELECT CPF FROM CANAL);
 
--- 3. Qual é a média de duração dos conteúdos publicados por cada canal, juntamente com seu nome e sua descrição?
+
+-- 3. Qual é a média de duração dos conteúdos publicados por cada canal, juntamente com seu nome e sua descrição? (INNER JOIN)
+
 SELECT C.NOME_CANAL, C.DESCRICAO, AVG(CT.DURACAO) AS MEDIA_DURACAO
 FROM CANAL C
 INNER JOIN CONTEUDO CT ON C.CPF = CT.CPF_STREAMER AND C.NOME_CANAL = CT.NOME_CANAL
@@ -18,18 +22,19 @@ GROUP BY C.NOME_CANAL, C.DESCRICAO;
 
 
 -- 4. Projete O nome e o CPF dos espectadores que não assinam nenhum canal (LEFT OUTER JOIN)
+
 SELECT E.CPF, E.NOME
 FROM ESPECTADOR E LEFT OUTER JOIN ASSINA A ON E.CPF = A.CPF_ESP
 WHERE A.CPF_STR IS NULL
 
 
--- 5. Projetar o título e o número de visualizações do conteúdo com id = 2 (SUBSELECT ESCALAR)
+-- 5. Projetar o título e o número de visualizações do conteúdo com id = 2 (SUBCONSULTA TIPO ESCALAR)
 
 SELECT C.TITULO, (SELECT COUNT(*) FROM VISUALIZA V WHERE V.ID = C.ID) AS NUM_VISUALIZACOES
 FROM CONTEUDO C
 WHERE C.ID = 2;
 
--- 6. Projetar o nome dos espectadores que assinam o Canal de João (SUBSELECT LINHA)
+-- 6. Projetar o nome dos espectadores que assinam o Canal de João (SUBCONSULTA TIPO TABELA)
 
 SELECT E.NOME AS NOME_ESPECTADOR
 FROM ESPECTADOR E
@@ -39,7 +44,7 @@ WHERE E.CPF IN (
     WHERE A.NOME_CANAL = 'Canal de João'
 );
 
--- 7. Projetar os os espectadores que assinam pelo menos um canal e que também possuem comentários em algum conteúdo
+-- 7. Projetar os os espectadores que assinam pelo menos um canal e que também possuem comentários em algum conteúdo (SUBCONSULTA TIPO TABELA)
 
 SELECT E.CPF, E.NOME
 FROM ESPECTADOR E
@@ -83,7 +88,7 @@ WHERE NOT EXISTS (
 );
 
 
--- 11. Projete os nomes dos espectadores que têm pelo menos uma categoria de interesse associada. (Subconsulta do Tipo Tabela)
+-- 11. Projete os nomes dos espectadores que têm pelo menos uma categoria de interesse associada. (SUBCONSULTA TIPO TABELA)
 
 SELECT E.NOME
 FROM ESPECTADOR E
@@ -99,7 +104,7 @@ GROUP BY EM.CPF
 HAVING COUNT(EM.EMAIL) > 1;
 
 
--- 13. Identifique os espectadores que não seguem outros espectadores e não assinam nenhum canal. (Antijoin)
+-- 13. Identifique os espectadores que não seguem outros espectadores e não assinam nenhum canal. (ANTI-JOIN)
 
 SELECT E.CPF
 FROM ESPECTADOR E
@@ -115,13 +120,13 @@ AND NOT EXISTS (
 );
 
 
--- 14. Projete o nome dos espectadores que são assinantes do canal do streamer que possui o e-mail breninho@gmail.com. (Junção interna, Subconsulta)
+-- 14. Projete o nome dos espectadores que são assinantes do canal do streamer cujo email é "breninho@gmail.com". (JUNÇÃO, SUBCONSULTA TIPO LINHA)
 
 SELECT E.NOME
 FROM ESPECTADOR E INNER JOIN ASSINA A ON E.CPF = A.CPF_ESP
 WHERE (A.CPF_STR, A.NOME_CANAL) = (SELECT C.CPF, C.NOME_CANAL
-						   		FROM CANAL C INNER JOIN STREAMER S ON C.CPF = S.CPF
-                           		WHERE S.EMAIL = 'breninho@gmail.com')
+						   		    FROM CANAL C INNER JOIN STREAMER S ON C.CPF = S.CPF
+                           		    WHERE S.EMAIL = 'breninho@gmail.com')
 
 
 -- 15. Projete o nome dos espectadores que visualizaram um conteúdo ou assinaram um canal (UNION)
@@ -158,16 +163,17 @@ WHERE S.CPF_SEGUIDO IN (
 );
 
 
---17. Identifique quais espectadores comentaram mais de uma vez no mesmo conteúdo
+--17. Identifique quais espectadores comentaram mais de uma vez no mesmo conteúdo(GROUP BY, HAVING)
 
 SELECT C.CPF, C.ID_CONTEUDO
 FROM COMENTARIOS C
 GROUP BY C.CPF, C.ID_CONTEUDO
 HAVING COUNT(*) > 1;
 
--- Identifique o nome dos espectadores que possuem alguma categoria de interesse em comum com alguma categoria de canais de streamers
+--18. Identifique todos os canais que não possuem nenhum assinante (MINUS)
 
-SELECT E.NOME, COUNT(*)
-FROM ESPECTADOR E INNER JOIN CATEGORIA_INTERESSE CI ON E.CPF=CI.CPF
-WHERE CI.CATEGORIA_INTERESSE IN (SELECT CA.CATEGORIA FROM CATEGORIA CA)
-GROUP BY E.NOME
+SELECT C.CPF, C.NOME_CANAL
+FROM CANAL C
+MINUS
+SELECT A.CPF_STR, A.NOME_CANAL
+FROM ASSINA A;	
